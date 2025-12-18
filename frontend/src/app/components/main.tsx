c098c538d3b6b4924ab6986058ab7b242825955f
juan galviz <marulandajuan292007@gmail.com>
2025-12-17 19:45:28 -0500

    cambio en el background del banner, por un fondo con un pillar en diagonal color rojo pasando a rojo oscuro, con un fondo negro, aparte un degradado debajo conectando con el resto de la pagina

--- frontend/src/app/components/main.tsx
+++ frontend/src/app/components/main.tsx
@@ -9,9 +9,27 @@ import TechSection from "./home/TechSection";
 
 const Main = () => {
   return (
-    <section className="flex flex-col gap-20">
-      <HeroBanner />
+    <section className="flex flex-col">
+      <HeroBanner
+        topColor="#3b0202"
+        bottomColor="#ff000d"
+        intensity={1.6}
+        rotationSpeed={0.4}
+        glowAmount={0.003}
+        pillarWidth={3.3}
+        pillarHeight={0.4}
+        noiseIntensity={0.4}
+        pillarRotation={54}
+        interactive={false}
+        mixBlendMode="normal"
+        bannerHeight="75vh"
+      />
+
+      {/* Degradado de transición (suavizado con varias paradas de color) */}
+      <div className="h-32 smooth-transition-gradient" />
+
       <CategoriesGrid />
+
       <div className="max-w-7xl mx-auto bg-white px-4 sm:px-10 py-16 flex flex-col gap-20">
         <FeaturedProducts />
         <BestSeller />
@@ -23,3 +41,4 @@ const Main = () => {
 };
 
 export default Main;
+
